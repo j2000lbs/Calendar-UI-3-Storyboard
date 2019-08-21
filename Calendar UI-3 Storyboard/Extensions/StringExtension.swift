7a5220dd7287a855be793ff76ba5319ffb417279
j2000lbs <j2000lbs@gmail.com>
2019-08-21 13:14:22 -0500

    String Extension

---
+++ Calendar UI-3 Storyboard/Extensions/StringExtension.swift
@@ -0,0 +1,23 @@
+//
+//  StringExtension.swift
+//  Calendar UI-3 Storyboard
+//
+//  Created by Joel Ton on 8/20/19.
+//  Copyright © 2019 RAMJETApps. All rights reserved.
+//
+
+import Foundation
+
+// Get date from string
+extension String {
+	static var dateFormatter: DateFormatter = {
+		let formatter = DateFormatter()
+		formatter.dateFormat = "MM-dd-yyyy"
+		return formatter
+	}()
+	
+	
+	var date: Date? {
+		return String.dateFormatter.date(from: self)
+	}
+}
